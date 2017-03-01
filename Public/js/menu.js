85fa3e4667d8df687685681a095c719bdf30740f
Zak <zwrawr@gmail.com>
2017-03-01 12:34:44 +0000

    Animated header navbar

--- Public/js/menu.js
+++ Public/js/menu.js
@@ -3,6 +3,18 @@ function toggleNavbar(){
     console.log("clicked");
     var navbar = document.getElementById("navbar-togglable");
 
-    navbar.classList.toggle("toggled-closed");
-
+    //navbar.classList.toggle("toggled-closed");
+    if (navbar.classList.contains("toggled-closed"))
+    {
+        navbar.classList.remove("toggled-closed");
+        navbar.classList.add("toggled-open");
+    }
+    else if (navbar.classList.contains("toggled-open"))
+    {
+        navbar.classList.remove("toggled-open");
+        navbar.classList.add("toggled-closed");
+    }
+    else {
+        navbar.classList.add("toggled-open");
+    }
 }
