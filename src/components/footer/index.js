3e0152d7dd402fa16bcb9d7b44b481493540194d
Zak West <zwrawr@gmail.com>
2019-07-08 15:02:08 +0100

    fixed header

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -10,19 +10,21 @@ const zw = '/public/img/zw.svg';
 
 const Footer = () => (
 	<footer class={style.footer + ' ' + style.cf}>
-		<div class={style.left}>
-			<a href="/#app">
-				<img src={zw} alt="Zak West's Z.W. logo" />
-			</a>
-			<a href="https://creativecommons.org/licenses/by/3.0/" >
-				<img class={style.small} src={cc} alt="crative commons mark" />
-				<img class={style.small} src={by} alt="by attribution" />
-			</a>
-		</div>
-		<div class={style.right}>
-			<a href="https://github.com/zwrawr/www.zakwest.co.uk">
-				<img src={github} alt="Source on Github" />
-			</a>
+		<div class={style.width}>
+			<div class={style.left}>
+				<a href="/#app">
+					<img src={zw} alt="Zak West's Z.W. logo" />
+				</a>
+				<a href="https://creativecommons.org/licenses/by/3.0/" >
+					<img class={style.small} src={cc} alt="crative commons mark" />
+					<img class={style.small} src={by} alt="by attribution" />
+				</a>
+			</div>
+			<div class={style.right}>
+				<a href="https://github.com/zwrawr/www.zakwest.co.uk">
+					<img src={github} alt="Source on Github" />
+				</a>
+			</div>
 		</div>
 	</footer>
 );
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -3,27 +3,14 @@ import { Link } from 'preact-router/match';
 import style from './style';
 //import LazyImg from '../lazyimg';
 
-const base = '/public/img/header/header_';
-const srcset =
-`${base}720w.png 720w,
-${base}1080w.png 480w,
-${base}1500w.png 1500w,
-${base}2000w.png 2000w,`;
-
-const sizes = '100vw';
+const img = '/public/img/header/header.svg';
 
 const Header = () => (
 	<header class={style.header}>
 		<div class={style.top}>
-			<picture>
-				<source media="(max-width: 576px)" srcset={base+'576.png'} />
-				<source media="(max-width: 768px)" srcset={base+'768.png'} />
-				<source media="(max-width: 992px)" srcset={base+'992.png'} />
-				<source media="(max-width: 1200px)" srcset={base+'1200.png'} />
-				<source media="(min-width: 1201px)" srcset={base+'2560.png'} />
-				<img src={base+'1200.png'} />
-			</picture>
-			<img style={style.headerimgcontainer} src={base+'1080w.png'} srcset={srcset} sizes={sizes} alt="blue-green triangle background" />
+			<div style={style.imgcontainer}>
+				<img src={img} alt="blue-green triangle background" />
+			</div>
 			<Link href="/" class={style.text}>
 				<h1>Zak West</h1>
 				<h5>An Electronics and Computer Engineering student at the University of York.</h5>
